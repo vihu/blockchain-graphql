660f6ee0d280034038f54ffa20b9aa1f8379cefe
Rahul Garg <vihu89@gmail.com>
2019-10-15 17:37:13 -0700

    Add some comments

--- mix.exs
+++ mix.exs
@@ -33,16 +33,23 @@ defmodule BlockchainGraphql.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
+
+      # phoenix deps
       {:phoenix, "~> 1.4.10"},
       {:phoenix_pubsub, "~> 1.1"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
+
+
+      # blockcahin specific deps
       {:ranch, "~> 1.7.1", override: true},
       {:blockchain, git: "git@github.com:helium/blockchain-core.git", branch: "master"},
       {:cuttlefish, git: "https://github.com/helium/cuttlefish.git", branch: "develop", override: true},
       {:lager, "3.6.7", [env: :prod, repo: "hexpm", hex: "lager", override: true, manager: :rebar3]},
       {:h3, git: "https://github.com/helium/erlang-h3.git", branch: "master"},
+
+      # graphql deps
       {:absinthe_plug, "~> 1.4"},
       {:absinthe, "~> 1.4"}
     ]
